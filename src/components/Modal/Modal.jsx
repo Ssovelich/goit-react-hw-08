91ce34494a04aa464a53c1fc3ca61edc968111bd
SSovelich <163519655+Ssovelich@users.noreply.github.com>
2024-11-09 19:10:09 +0200

    fix contact name in modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -39,7 +39,7 @@ const Modal = ({ onCloseModal, contactId, onBackdropClick, name }) => {
     <div onClick={onBackdropClick} className={styles.backdrop}>
       <div className={styles.modal}>
         <p className={styles.text}>Do you want to delete the contact:</p>
-        <p className={styles.text}>{name}?</p>
+        <p className={styles.name}>{name}?</p>
         <div className={styles.wrapBtn}>
           <button onClick={onCloseModal} className={styles.btn} type="button">
             No
